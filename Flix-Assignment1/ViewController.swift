d2d72f28eda88eeda598d971dd1e8faf59cd548a
kripeshr22 <kranabha@students.pitzer.edu>
2022-02-25 00:53:21 -0800

    Initial Commit

---
+++ Flix-Assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix-Assignment1
+//
+//  Created by Kripesh Ranabhat on 2/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Flix-Assignment1UITests/Flix_Assignment1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flix_Assignment1UITestsLaunchTests.swift
+//  Flix-Assignment1UITests
+//
+//  Created by Kripesh Ranabhat on 2/25/22.
+//
+
+import XCTest
+
+class Flix_Assignment1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
